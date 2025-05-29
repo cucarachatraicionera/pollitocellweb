0515530501b51c9dae8f98ce7c359c3b5813964d
cucarachatraicionera <cucarachatraicionera@gmail.com>
2025-05-29 10:22:44 -0500

    error

--- src/components/Home/Countdown/index.tsx
+++ src/components/Home/Countdown/index.tsx
@@ -20,7 +20,9 @@ const CounDown = () => {
   };
 
   useEffect(() => {
+    // @ts-ignore
     const interval = setInterval(() => getTime(deadline), 1000);
+
     return () => clearInterval(interval);
   }, []);
 
@@ -30,75 +32,96 @@ const CounDown = () => {
         <div className="relative overflow-hidden z-1 rounded-lg bg-[#D0E9F3] p-4 sm:p-7.5 lg:p-10 xl:p-15">
           <div className="max-w-[422px] w-full">
             <span className="block font-medium text-custom-1 text-blue mb-2.5">
-              ¡No te lo pierdas!
+              Don’t Miss!!
             </span>
 
             <h2 className="font-bold text-dark text-xl lg:text-heading-4 xl:text-heading-3 mb-3">
-              Oferta limitada en PollitoCell
+              Enhance Your Music Experience
             </h2>
 
-            <p>
-              Aprovecha nuestros descuentos especiales en celulares Apple, accesorios y servicio técnico. ¡Solo hasta fin de año!
-            </p>
+            <p>The Havit H206d is a wired PC headphone.</p>
 
-            {/* Contador regresivo */}
-            <div className="flex flex-wrap gap-6 mt-6">
+            {/* <!-- Countdown timer --> */}
+            <div
+              className="flex flex-wrap gap-6 mt-6"
+              x-data="timer()"
+              x-init="countdown()"
+            >
+              {/* <!-- timer day --> */}
               <div>
-                <span className="min-w-[64px] h-14.5 font-semibold text-xl lg:text-3xl text-dark rounded-lg flex items-center justify-center bg-white shadow-2 px-4 mb-2">
-                  {days < 10 ? "0" + days : days}
+                <span
+                  className="min-w-[64px] h-14.5 font-semibold text-xl lg:text-3xl text-dark rounded-lg flex items-center justify-center bg-white shadow-2 px-4 mb-2"
+                  x-text="days"
+                >
+                  {" "}
+                  {days < 10 ? "0" + days : days}{" "}
                 </span>
                 <span className="block text-custom-sm text-dark text-center">
-                  Días
+                  Days
                 </span>
               </div>
 
+              {/* <!-- timer hours --> */}
               <div>
-                <span className="min-w-[64px] h-14.5 font-semibold text-xl lg:text-3xl text-dark rounded-lg flex items-center justify-center bg-white shadow-2 px-4 mb-2">
-                  {hours < 10 ? "0" + hours : hours}
+                <span
+                  className="min-w-[64px] h-14.5 font-semibold text-xl lg:text-3xl text-dark rounded-lg flex items-center justify-center bg-white shadow-2 px-4 mb-2"
+                  x-text="hours"
+                >
+                  {" "}
+                  {hours < 10 ? "0" + hours : hours}{" "}
                 </span>
                 <span className="block text-custom-sm text-dark text-center">
-                  Horas
+                  Hours
                 </span>
               </div>
 
+              {/* <!-- timer minutes --> */}
               <div>
-                <span className="min-w-[64px] h-14.5 font-semibold text-xl lg:text-3xl text-dark rounded-lg flex items-center justify-center bg-white shadow-2 px-4 mb-2">
-                  {minutes < 10 ? "0" + minutes : minutes}
+                <span
+                  className="min-w-[64px] h-14.5 font-semibold text-xl lg:text-3xl text-dark rounded-lg flex items-center justify-center bg-white shadow-2 px-4 mb-2"
+                  x-text="minutes"
+                >
+                  {minutes < 10 ? "0" + minutes : minutes}{" "}
                 </span>
                 <span className="block text-custom-sm text-dark text-center">
-                  Minutos
+                  Minutes
                 </span>
               </div>
 
+              {/* <!-- timer seconds --> */}
               <div>
-                <span className="min-w-[64px] h-14.5 font-semibold text-xl lg:text-3xl text-dark rounded-lg flex items-center justify-center bg-white shadow-2 px-4 mb-2">
-                  {seconds < 10 ? "0" + seconds : seconds}
+                <span
+                  className="min-w-[64px] h-14.5 font-semibold text-xl lg:text-3xl text-dark rounded-lg flex items-center justify-center bg-white shadow-2 px-4 mb-2"
+                  x-text="seconds"
+                >
+                  {seconds < 10 ? "0" + seconds : seconds}{" "}
                 </span>
                 <span className="block text-custom-sm text-dark text-center">
-                  Segundos
+                  Seconds
                 </span>
               </div>
             </div>
-            {/* Fin contador */}
+            {/* <!-- Countdown timer ends --> */}
 
             <a
               href="#"
               className="inline-flex font-medium text-custom-sm text-white bg-blue py-3 px-9.5 rounded-md ease-out duration-200 hover:bg-blue-dark mt-7.5"
             >
-              ¡Ver ofertas!
+              Check it Out!
             </a>
           </div>
 
+          {/* <!-- bg shapes --> */}
           <Image
             src="/images/countdown/countdown-bg.png"
-            alt="fondo gráfico"
+            alt="bg shapes"
             className="hidden sm:block absolute right-0 bottom-0 -z-1"
             width={737}
             height={482}
           />
           <Image
             src="/images/countdown/countdown-01.png"
-            alt="producto en promoción"
+            alt="product"
             className="hidden lg:block absolute right-4 xl:right-33 bottom-4 xl:bottom-10 -z-1"
             width={411}
             height={376}
